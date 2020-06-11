8ab38a9fa1c7153262d36c23586d5fdc7b87528d
xjq <lebnizjoan@gmail.com>
2020-06-11 09:34:12 +0800

    refactor: 删除googleinsight

--- plugins/metrics.js
+++ plugins/metrics.js
@@ -4,7 +4,5 @@ export default async function plugin({ succeed, fail, chalk }, options = {}) {
   //无头浏览器检测
   await handleMetrics({ chalk, succeed, fail })
 
-  //google insight
-  // await handleGoogleInsight({ chalk, succeed, fail })
   succeed(chalk.white(`性能指标检测完毕`))
 }
